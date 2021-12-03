cdda6db91539679653ced6136172d4fedaf8b4a0
mu weiye <muwy1995@outlook.com>
2021-12-04 02:09:58 +0800

    format

--- pm_entity/src/main/java/com/pm/entity/PluploadService.java
+++ pm_entity/src/main/java/com/pm/entity/PluploadService.java
@@ -1,14 +1,9 @@
 package com.pm.entity;
 
-import com.pm.entity.FileUp;
-import com.pm.entity.Plupload;
-import com.pm.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-import javax.servlet.http.HttpServletRequest;
 import java.io.*;
 import java.util.Iterator;
 import java.util.List;
@@ -18,85 +13,79 @@ import java.util.List;
  */
 public class PluploadService {
 
-    public static void upload(Plupload plupload, File pluploadDir,String fileName){
+    public static void upload(Plupload plupload, File pluploadDir, String fileName) {
 //        String fileName = ""+System.currentTimeMillis()+plupload.getName();//在服务器内生成唯一文件名
         String unuse = "unuser";
-        upload(plupload,pluploadDir,fileName,unuse);
+        upload(plupload, pluploadDir, fileName, unuse);
     }
 
-    private static void upload(Plupload plupload,File pluploadDir,String fileName,String unuse){
+    private static void upload(Plupload plupload, File pluploadDir, String fileName, String unuse) {
 
         int chunks = plupload.getChunks();//用户上传文件被分隔的总块数
         int nowChunk = plupload.getChunk();//当前块，从0开始
 
         //这里Request请求类型的强制转换可能出错，配置文件中向SpringIOC容器引入multipartResolver对象即可。
-        MultipartHttpServletRequest multipartHttpServletRequest  = (MultipartHttpServletRequest)plupload.getRequest();
+        MultipartHttpServletRequest multipartHttpServletRequest = (MultipartHttpServletRequest) plupload.getRequest();
         //调试发现map中只有一个键值对
         MultiValueMap map = multipartHttpServletRequest.getMultiFileMap();
 
-        if(map!=null){
-            try{
+        if (map != null) {
+            try {
                 Iterator<String> iterator = map.keySet().iterator();
-                while(iterator.hasNext()){
+                while (iterator.hasNext()) {
 
                     String key = iterator.next();
                     List<MultipartFile> multipartFileList = (List<MultipartFile>) map.get(key);
 
-                    for(MultipartFile multipartFile:multipartFileList){//循环只进行一次
+                    for (MultipartFile multipartFile : multipartFileList) {//循环只进行一次
 
                         plupload.setMultipartFile(multipartFile);//手动向Plupload对象传入MultipartFile属性值
-                        File targetFile = new File(pluploadDir+"/"+fileName);//新建目标文件，只有被流写入时才会真正存在
-                        if(chunks>1){//用户上传资料总块数大于1，要进行合并
+                        File targetFile = new File(pluploadDir + "/" + fileName);//新建目标文件，只有被流写入时才会真正存在
+                        if (chunks > 1) {//用户上传资料总块数大于1，要进行合并
 
-                            File tempFile = new File(pluploadDir.getPath()+"/"+multipartFile.getName());
+                            File tempFile = new File(pluploadDir.getPath() + "/" + multipartFile.getName());
                             //第一块直接从头写入，不用从末端写入
-                            savePluploadFile(multipartFile.getInputStream(),tempFile,nowChunk==0?false:true);
+                            savePluploadFile(multipartFile.getInputStream(), tempFile, nowChunk != 0);
 
-                            if(chunks-nowChunk==1){//全部块已经上传完毕，此时targetFile因为有被流写入而存在，要改文件名字
+                            if (chunks - nowChunk == 1) {//全部块已经上传完毕，此时targetFile因为有被流写入而存在，要改文件名字
                                 tempFile.renameTo(targetFile);
                             }
-                        }
-                        else{
+                        } else {
                             //只有一块，就直接拷贝文件内容
                             multipartFile.transferTo(targetFile);
                         }
                     }
                 }
-            }
-            catch (IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
     }
-    private static void savePluploadFile(InputStream inputStream,File tempFile,boolean flag){
+
+    private static void savePluploadFile(InputStream inputStream, File tempFile, boolean flag) {
         OutputStream outputStream = null;
         try {
-            if(flag==false){
+            if (!flag) {
                 //从头写入
                 outputStream = new BufferedOutputStream(new FileOutputStream(tempFile));
-            }
-            else{
+            } else {
                 //从末端写入
-                outputStream = new BufferedOutputStream(new FileOutputStream(tempFile,true));
+                outputStream = new BufferedOutputStream(new FileOutputStream(tempFile, true));
             }
             byte[] bytes = new byte[1024];
             int len = 0;
-            while ((len = (inputStream.read(bytes)))>0){
-                outputStream.write(bytes,0,len);
+            while ((len = (inputStream.read(bytes))) > 0) {
+                outputStream.write(bytes, 0, len);
             }
-        }
-        catch (FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
-        catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
-        }
-        finally {
-            try{
+        } finally {
+            try {
                 outputStream.close();
                 inputStream.close();
-            }
-            catch (IOException e){
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
--- pm_service/src/main/java/com/pm/service/impl/BigfileServiceImpl.java
+++ pm_service/src/main/java/com/pm/service/impl/BigfileServiceImpl.java
@@ -2,11 +2,12 @@ package com.pm.service.impl;
 
 
 import com.pm.dao.FileUpMapper;
-import com.pm.dao.PictureUserMapper;
-import com.pm.entity.*;
+import com.pm.entity.FileUp;
+import com.pm.entity.Plupload;
+import com.pm.entity.PluploadService;
+import com.pm.entity.User;
 import com.pm.service.BigfileService;
 import com.pm.utils.FFMPEG;
-import com.pm.utils.ImageMarkLogoByIcon;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,10 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.util.HashMap;
-import java.util.List;
 import java.util.UUID;
-
-import static org.aspectj.weaver.tools.cache.SimpleCacheFactory.path;
 
 /**
  * Created by Dell on 2017/8/24.
@@ -30,43 +28,46 @@ public class BigfileServiceImpl implements BigfileService {
     @Autowired
     FileUpMapper fileUpMapper;
 
-    private String  filePath;
-    private String  pluploadname;
+    private String filePath;
+    private String pluploadname;
 
     private String filePathshow;
     private String FileDir;
 
 
-    @Transactional
+    @Override
+    @Transactional(rollbackFor = Exception.class)
     public void uploadvideo(Plupload plupload, HttpServletRequest request, HttpServletResponse response) {
-D:
 //        String FileDir = "pluploadDir";//文件保存的文件夹
-
         FileDir = "/software/apache-tomcat-8.0.47/webapps/ROOT/picture_bed/";
 
-        plupload.setRequest(request);//手动传入Plupload对象HttpServletRequest属性
+        //手动传入Plupload对象HttpServletRequest属性
+        plupload.setRequest(request);
 
 //        Long userId = ((User)request.getSession().getAttribute("user")).getId();
 
         //文件存储绝对路径,会是一个文件夹，项目相应Servlet容器下的"pluploadDir"文件夹，还会以用户唯一id作划分
         File dir = new File(FileDir);
 
-        if(!dir.exists()){
+        if (!dir.exists()) {
             dir.mkdirs();//可创建多级目录，而mkdir()只能创建一级目录
         }
 
-        filePath = ""+ UUID.randomUUID().toString()+"."+plupload.getName().substring(plupload.getName().lastIndexOf(".") + 1); //在服务器内生成唯一文件名
-        filePathshow = ""+UUID.randomUUID().toString()+"."+plupload.getName().substring(plupload.getName().lastIndexOf(".") + 1);//加水印视频
+        //在服务器内生成唯一文件名
+        filePath = UUID.randomUUID() + "." + plupload.getName().substring(plupload.getName().lastIndexOf(".") + 1);
+        //加水印视频
+        filePathshow = UUID.randomUUID() + "." + plupload.getName().substring(plupload.getName().lastIndexOf(".") + 1);
 
 
         pluploadname = plupload.getName();
 
         //开始上传文件
-        PluploadService.upload(plupload, dir,filePath);
+        PluploadService.upload(plupload, dir, filePath);
 
     }
 
-    @Transactional
+    @Override
+    @Transactional(rollbackFor = Exception.class)
     public void uploadvideoRecord(HttpServletRequest request) {
 
 
@@ -86,16 +87,16 @@ D:
 
         fileUpMapper.insert(fileUp);
 
-        String pathVideo = FileDir+filePath;
-        String pathShowVideo = FileDir+filePathshow;
+        String pathVideo = FileDir + filePath;
+        String pathShowVideo = FileDir + filePathshow;
 
 
-        HashMap<String, String> dto=new HashMap<String, String>();
+        HashMap<String, String> dto = new HashMap<>();
         dto.put("ffmpeg_path", "D:\\software\\tool\\ffmpeg\\bin\\ffmpeg.exe");//必填
         dto.put("input_path", pathVideo);//必填
-        dto.put("video_converted_path", pathShowVideo );//必填
+        dto.put("video_converted_path", pathShowVideo);//必填
         dto.put("logo", "D\\\\:/software/tool/ffmpeg/input/logo.png");//可选(注意windows下面的logo地址前面要写4个反斜杠,如果用浏览器里面调用servlet并传参只用两个,如 d:\\:/ffmpeg/input/logo.png)
-        String secondsString=  new FFMPEG().videoTransfer(dto);
+        String secondsString = new FFMPEG().videoTransfer(dto);
 //        System.out.println("转换共用:"+secondsString+"秒");
 //
 
